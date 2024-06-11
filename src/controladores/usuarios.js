f602c138bd34fd3486870bd945218dce6fba709c
italo macedo <macedoitalo21@gmail.com>
2024-06-10 21:16:44 -0300

    Validação de senha com bcrypt

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,10 +1,12 @@
 const pool = require('../conexao')
+const bcrypt = require('bcrypt')
 
 const cadastrarUsuario = async (req,res) => {
     const {nome,email,senha} = req.body
 
     try {
-        const novoUsuario = await pool.query('insert into usuarios (nome,email,senha) value ($1, $2, $3) returning *', [nome,email,senha])
+        const senhaCriptografada = await bcrypt.hash(senha,10)
+        const novoUsuario = await pool.query('insert into usuarios (nome,email,senha) value ($1, $2, $3) returning *', [nome,email,senhaCriptografada])
         return res.status(201).json(novoUsuario.rows[0])
     } 
     
@@ -13,6 +15,29 @@ const cadastrarUsuario = async (req,res) => {
     }
 }
 
+const login = async (req,res) => {
+    const {email,senha} = req.body
+    try {
+        const usuario = await pool.query('select * from usuarios where email = $1', [email])
+        
+        if (usuario.rowCount < 1) {
+            return res.status(404).json({mensagem: 'Email ou senha invalida'})
+        }
+
+        const senhaValida = await bcrypt.compare(senha, usuario.rows[0].senha)
+
+        if (!senhaValida) {
+            return res.status(400).json({mensagem: 'Email ou senha invalida!'})
+        }
+
+        return res.json({mensagem: 'Usuario autenticado'})
+    
+    } catch (error) {
+        return res.status(500).json({mensagem:'Erro interno do servidor'})
+    }
+}
+
 module.exports = {
-    cadastrarUsuario
+    cadastrarUsuario,
+    login
 }
